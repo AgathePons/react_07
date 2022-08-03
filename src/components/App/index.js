5a681bab6d1defc0ec8e66bbd0af4b80203a7675
AgathePons <ponsagathe37@gmail.com>
2022-08-03 11:22:41 +0200

    nav from menu done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,9 @@
 // == Import
 import { useState } from 'react';
 import axios from 'axios';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+import './styles.scss';
 
 import SearchBar from 'src/components/SearchBar';
 import ReposResults from 'src/components/ReposResults';
@@ -17,11 +20,13 @@ import {
 function App() {
   const [searchInputText, setSearchInputText] = useState('');
   const [reposData, setReposData] = useState([]);
+  const [searchResultCount, setSearchResultCount] = useState(0);
 
   const handleLoadData = async () => {
     try {
       const response = await axios.get(`https://api.github.com/search/repositories?q=${searchInputText}`);
       console.log(`url : https://api.github.com/search/repositories?q=${searchInputText}`);
+      setSearchResultCount(response.data.total_count);
       setReposData(response.data.items);
     }
     catch (error) {
@@ -43,36 +48,52 @@ function App() {
     <div className="app">
       <div className="ui container">
         <header>
-          <Header as="h1">
-            <Icon name="github" size="big" />
+          <Header as="h1" textAlign="center">
+            <Icon name="github" size="massive" />
             GitHub
           </Header>
         </header>
         <Menu>
-          <Menu.Item
-            name="Recherche"
-          >
-            Recherche
-          </Menu.Item>
-          <Menu.Item
-            name="FAQ"
-          >
-            FAQ
-          </Menu.Item>
+          <NavLink to="/">
+            <Menu.Item
+              name="Recherche"
+            >
+              Recherche
+            </Menu.Item>
+          </NavLink>
+          <NavLink to="/faq">
+            <Menu.Item
+              name="FAQ"
+            >
+              FAQ
+            </Menu.Item>
+          </NavLink>
         </Menu>
-        <Card fluid>
-          <Card.Content>
-            <Card.Description>
-              <SearchBar
-                searchInputText={searchInputText}
-                onSearchInputChange={handleSearchInputChange}
-                onSearchInputSubmit={handleSearchInputSubmit}
-              />
-            </Card.Description>
-          </Card.Content>
-        </Card>
-        <Card.Group>
-          {
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <>
+                <Card fluid>
+                  <Card.Content>
+                    <Card.Description>
+                      <SearchBar
+                        searchInputText={searchInputText}
+                        onSearchInputChange={handleSearchInputChange}
+                        onSearchInputSubmit={handleSearchInputSubmit}
+                      />
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+                <Card fluid>
+                  <Card.Content>
+                    <Card.Description>
+                      La recherche a donné {searchResultCount} résultats
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+                <Card.Group>
+                  {
             reposData.map((repo) => (
               <ReposResults
                 key={repo.id}
@@ -84,11 +105,21 @@ function App() {
               />
             ))
           }
-        </Card.Group>
+                </Card.Group>
+              </>
+            )}
+          />
 
-        <FaqPage />
+          <Route
+            path="/faq"
+            element={(
+              <FaqPage />
+            )}
+          />
 
-        <NotFound />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       </div>
 
     </div>
